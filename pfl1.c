1ee583824a8624f9762c11320e83ec1d06a47f33
mfergu <mfergu3@g.clemson.edu>
2017-06-02 17:38:25 -0400

    added some metadata

--- pfl1.c
+++ pfl1.c
@@ -1,8 +1,16 @@
+//http://www.bradrodriguez.com/papers/ms/pat4th-c.html
+//http://bitsquid.blogspot.com/2015/08/allocation-adventures-3-buddy-allocator.html
 #include "pfl1.h"
+
 
 
 struct region_node *get_free_node(){
 
+	struct region_node* temp = *head;
+	unsigned int j = 0;
+	bool node_found = false;
+	while( j < NUM_NODES && !node_found) {
+		
 	return;
 };
 
