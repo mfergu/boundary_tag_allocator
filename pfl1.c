595ba31d6cde13521b117b198032ff7e6b38cb19
mfergu <mfergu3@g.clemson.edu>
2017-06-04 08:11:30 -0400

    removing and coalescing all that's left

--- pfl1.c
+++ pfl1.c
@@ -1,7 +1,6 @@
 
 #include "pfl1.h"
 #include <stdlib.h>
-#include <stdlib.h>
 
 typedef struct region_node T;
 
@@ -23,9 +22,6 @@ void return_node( T* n ){
 	n->region_state = FREE;
 }
 
-//For a positive region size, alloc() searches the memory region list in order,
-//looking for a free memory region that is equal in size or larger than the
-//requested size.
 unsigned char *alloc( int size ){
 
 	if( !(size > 0)){
@@ -44,9 +40,9 @@ int release( unsigned char *ptr ){
 
 T* rec_get_free_node( T* temp){
 
-	if( temp->region_state != FREE) {
-		if( !(unsigned long)temp > (unsigned long)(node + NUM_NODES - 1)) {
-			rec_get_free_node( temp + 1);
+	if( temp->node_state != FREE) {
+		if( !((unsigned long)temp > (unsigned long)(node + NUM_NODES - 1))) {
+			return rec_get_free_node( temp + 1);
 		} else {
 			return NULL;
 		}
@@ -81,13 +77,7 @@ T* rec_find_spot(int size_needed, T* temp){
 					return temp;
 				}
 				//additional nodes are available
-				//divide the spot between two nodes
-				int total_size = temp->size;
-				
-				//initialize the newly populated node to match requested size
-				temp->size = size_needed;
 				add_node( temp, open, size_needed);
-
 				return temp;
 			}
 		} else {
@@ -101,26 +91,24 @@ T* rec_find_spot(int size_needed, T* temp){
 	}
 }
 
-//keep all the nodes in a single list in the order of each memory region's
-//starting address. This ordering of free and allocated memory
-//regions will simplify the coalescing logic for free memory regions
-//when a memory region release occurs.
 void add_node(T* temp, T* open, int size_requested){
-
 	if( open != NULL) {
+		//divide the spot between two nodes
+		//initialize the newly populated node to match requested size
 		open->node_state = IN_USE;
 		open->region_state = FREE;
-		open->size = temp->size + size_requested;
+
+		open->size = temp->size - size_requested;
+		temp->size = size_requested;
 		
+		open->start_address = temp->start_address + temp->size;
+
 		open->prev = temp;
-		temp->next = open;
-		open->next = temp->next;
 		temp->next->prev = open;
+		open->next = temp->next;
+		temp->next = open;
 	}
-	
-	temp->node_state = IN_USE;
 	temp->region_state = IN_USE;
-	temp->size = size_requested;
 }
 
 
